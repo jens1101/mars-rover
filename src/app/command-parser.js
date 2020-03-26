5d915d40eac7716a76230cffed7642326a2dda9a
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-03-26 15:34:06 +0200

    Added remaining files
    - Added all the files that are left for this project
    - Added TODO items for everything that is left to do

---
+++ src/app/command-parser.js
@@ -0,0 +1,2 @@
+// TODO: add functions here that parse a given set of commands into an array
+//  of functions that can be called one by one.
---
+++ src/app/main.js
@@ -0,0 +1,5 @@
+// TODO: add the ability to get input from the text area
+// TODO: parse the commands and execute each one. There should be a short delay
+//  between each command (say 400ms) so that the UI can update at a perceivable
+//  speed.
+// TODO: add rendering to the canvas
